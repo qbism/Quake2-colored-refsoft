5ea28627da3aeac1ba57e2f76035cdffddb70570
qbism <jeff@qbism.com>
2015-09-20 22:00:17 -0400

    1.1 cont'd: particle size basis 800x screen instead of 320x screen fix debug build

--- ref_soft/r_misc.c
+++ ref_soft/r_misc.c
@@ -90,8 +90,8 @@ void D_ViewChanged (void)
 	if (d_pix_min < 1)
 		d_pix_min = 1;
 
-	d_pix_max = (int)((float)r_refdef.vrect.width / (320.0 / 4.0) + 0.5);
-	d_pix_shift = 8 - (int)((float)r_refdef.vrect.width / 320.0 + 0.5);
+	d_pix_max = (int)((float)r_refdef.vrect.width / (800.0 / 4.0) + 0.5); //qb: smaller particles.  was 320.0
+	d_pix_shift = 8 - (int)((float)r_refdef.vrect.width / 800.0 + 0.5); //qb: smaller particles.  was 320.0
 	if (d_pix_max < 1)
 		d_pix_max = 1;
 
